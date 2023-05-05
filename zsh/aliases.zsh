d7cea14847e403f0a8d13c1711a54bd937b18720
David Riosalido <driosalido@gmail.com>
2023-05-05 18:40:02 +0200

    Add lsd as colorize terminal util

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,8 @@
-alias lc='colorls -la'
-alias ls="colorls --sd -A"
+#alias lc='colorls -la'
+#alias ls="colorls --sd -A"
+alias ls='lsd'
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
 alias acki='ack --ignore-directory=.terragrunt-cache --ignore-directory=build'
