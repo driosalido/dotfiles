c38ca1b5c8be8f5bf57139dcf3a0254d1011be11
driosalido@gmail.com <driosalido@gmail.com>
2023-11-09 10:46:37 +0100

    Various updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,3 +7,5 @@ alias lla='ls -la'
 alias lt='ls --tree'
 alias acki='ack --ignore-directory=.terragrunt-cache --ignore-directory=build'
 alias cat='bat --style=plain'
+alias j=z
+alias ap='opal aws:identity | jq ".Arn" 2>/dev/null | cut -d ":" -f 5,6'
--- zsh/intenthq.zsh
+++ zsh/intenthq.zsh
@@ -1,13 +1,20 @@
 # eval "$(direnv hook zsh)"
-# export PATH="$HOME/git/hello/local-env/bin:$HOME/git/infrastructure/scripts/:$PATH"
-# #export OKTA_AWS_ROLE_TO_ASSUME=CA-EngineerAccessRole
-# export OKTA_AWS_ROLE_TO_ASSUME=CA-InfrastructureAccessRole
-# #export OKTA_AWS_ROLE_TO_ASSUME=CA-AdministratorAccessRole
-# export GITHUB_TOKEN=`security find-generic-password -s github-token-driosalido -a driosalido -w`
-# export OKTA_PASSWORD_CMD="security find-generic-password -a david.riosalido@intenthq.com -s okta-aws-cli -w"
-# export AWS_PROFILE=$(cat ~/.aws/active_profile)
-# export TF_PLUGIN_CACHE_DIR="/Users/driosalido/.terraform.d/plugin-cache"
+#PATHS
+export PATH="$HOME/git/hello/local-env/bin:$HOME/git/infrastructure/scripts/:$PATH"
 export PATH="/usr/local/opt/python@3.9/bin:$PATH"
 export JEP_PATH="/usr/local/lib/python3.9/site-packages/jep/"
 export PATH="/Users/driosalido/git/config-validations:$PATH"
 export PATH="/usr/local/opt/ruby/bin:$PATH"
+
+#GITHUB TOKEN
+export GITHUB_TOKEN=`security find-generic-password -s github-token-intenthq -a driosalido -w`
+#export GITHUB_TOKEN="$(op read op://Private/github-token-intenthq/credential)"
+
+#AWS PROFILE
+# export AWS_PROFILE=$(cat ~/.aws/active_profile)
+export AWS_PROFILE=opal
+export AWS_DEFAULT_PROFILE=opal
+
+#TERRAFORM
+export TF_PLUGIN_CACHE_DIR="/Users/driosalido/.terraform.d/plugin-cache"
+
